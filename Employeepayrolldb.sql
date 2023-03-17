6d154d079c90ec3c863f8a461a386402ffd2b75c
chirrashruthigoud <chirrashruthigoud4@gmail.com>
2023-03-17 12:05:58 +0530

    Update salary of employee

--- Employeepayrolldb.sql
+++ Employeepayrolldb.sql
@@ -1,22 +1,10 @@
 use Payroll_Service;
+select  * from employeep
 
-create database payroll_service
-
-
-create table employeep
-(
-Id int identity primary key,
-Name varchar(250),
-BasicPay float, 
-Gender char(1),
-PhoneNumber varchar(250),
-Address varchar(250),
-Department varchar(250), 
-Deductions float, 
-TaxablePay float, 
-IncomeTax float,
-NetPay float, 
-StartDate Date 
-
-)
-select*from employeep
+--update employee salary
+alter procedure SpAddEmployeeDetails
+@Name nvarchar(200),
+@Id int,
+@BasicPay float
+AS
+update employeep set BasicPay=@BasicPay where Name=@Name and Id= @Id
